f0ebdf1cdab701314754f518959e51a2df3c18bf
ashimsarkar1512 <ashimsarkar5558@gmail.com>
2024-11-08 17:49:44 +0600

    solve the problem-6

---
+++ assignment-1/dist/problem-6.js
@@ -0,0 +1,6 @@
+"use strict";
+function updateProfile(profile, update) {
+    return Object.assign(Object.assign({}, profile), update);
+}
+const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+console.log(updateProfile(myProfile, { age: 26 }));
